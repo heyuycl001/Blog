88ebe9df718e238635f2c8ad7b3c878f827d1196
heyuycl001 <heyuycl001@163.com>
2020-08-19 23:06:29 +0800

    6.js

---
+++ 小题练习/6.js
@@ -0,0 +1,19 @@
+var length = 10
+function fn () {
+  console.log(this.length)
+}
+
+var yideng = {
+  length: 5,
+  method: function (fn) {
+    fn()
+    arguments[0]()
+  }
+}
+
+yideng.method(fn, 1)
+
+/**
+ * 10
+ * 2
+ */
