a0f137902c5cdbd0234ae7b826c4649e5b323f50
heyuycl001 <heyuycl001@163.com>
2020-08-25 21:22:48 +0800

    10.js

--- js中的各种概念/闭包.js
+++ js中的各种概念/闭包.js
@@ -1,29 +1,39 @@
-for (var i = 0; i < 5; i++) {
-  setTimeout(() => {
-    console.log(i)
-  }, 0);
-}
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(() => {
+//     console.log(i)
+//   }, 0);
+// }
 
 
-for (var i = 0; i < 5; i++) {
-  (function (i) {
-    setTimeout(() => {
-      console.log(i)
-    }, 0);
-  })(i)
-}
+// for (var i = 0; i < 5; i++) {
+//   (function (i) {
+//     setTimeout(() => {
+//       console.log(i)
+//     }, 0);
+//   })(i)
+// }
 
 
-var fn = (function () {
+// var fn = (function () {
+//   // 这样声明的变量为模块私有变量，外界无法访问
+//   var foo = 0
+//   function fn () { }
+//   fn.prototype.bar = function bar () {
+//     return foo
+//   }
+//   return fn
+// })()
 
-  // 这样声明的变量为模块私有变量，外界无法访问
-  var foo = 0
 
-  function fn () { }
-
-  fn.prototype.bar = function bar () {
-    return foo
+function bar () {
+  var sum = 10
+  function bzz () {
+    return sum += 1
   }
-
-  return fn
-})()
+  window.bzz = bzz
+}
+bar()
+bzz()
+bzz()
+bzz()
+bzz()
---
+++ 小题练习/10.js
@@ -0,0 +1,39 @@
+// 手写简易call
+/**
+ *  首先考虑this，是谁调用，this就是谁。
+ */
+
+var foo = {
+  value: 1
+};
+function bar (a, b) {
+  console.log(a, b)
+  console.log(this.value);
+}
+
+/**
+ * 如果我们把函数放到foo里，
+ * 然后执行foo.bar()，此时this是不是已经绑定成功了
+ */
+
+Function.prototype.call2 = function call2 (content) {
+  content.fn = this
+  content.fn()
+  delete content.fn
+}
+// bar.call2(foo) // 此时this真的绑定成功了
+
+/**
+ * 下面考虑如何传参数
+ */
+Function.prototype.call2 = function call2 (content) {
+  const args = Array.from(arguments).slice(1)
+  content.fn = this
+  content.fn(...args)
+  delete content.fn
+}
+bar.call2(foo, 199, 200)
+
+/**
+ * 参数也绑定成功了
+ */
