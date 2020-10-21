b25323cdb13741b55785a3b3bdb4cce55841badb
heyu <heyuycl001@163.com>
2020-10-21 15:48:00 +0800

    EventBus

---
+++ vue/2.x/组件通信/EventBus/child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>child</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$EventBus.$on("foo", (val) => {
+      console.log(val);
+    });
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/2.x/组件通信/EventBus/child1.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>child1</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.$parent.$emit("foo", "这是我给兄弟传过去的");
+  },
+};
+</script>
+
+<style>
+</style>
