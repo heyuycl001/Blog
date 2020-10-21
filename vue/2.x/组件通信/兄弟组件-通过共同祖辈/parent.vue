16c5b8051a4aac87daac59086a7d3210117664f9
heyu <heyuycl001@163.com>
2020-10-21 14:51:20 +0800

    兄弟组件-通过共同祖辈

---
+++ vue/2.x/组件通信/兄弟组件-通过共同祖辈/parent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Child></Child>
+    <Child1></Child1>
+  </div>
+</template>
+
+<script>
+import Child from "./components/child";
+import Child1 from "./components/child1";
+
+export default {
+  name: "App",
+  components: {
+    Child,
+    Child1,
+  },
+};
+</script>
+
+<style>
+</style>
