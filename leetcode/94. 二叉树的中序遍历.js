80e0985b52e5b209a2fa053dcdf05459e0dba1aa
huanxin <heyuycl001@163.com>
2021-02-24 19:02:14 +0800

    94. 二叉树的中序遍历

---
+++ leetcode/94. 二叉树的中序遍历.js
@@ -0,0 +1,57 @@
+// 94. 二叉树的中序遍历
+/* 
+给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+
+示例 1：
+输入：root = [1,null,2,3]
+输出：[1,3,2]
+
+示例 2：
+输入：root = []
+输出：[]
+
+示例 3：
+输入：root = [1]
+输出：[1]
+*/
+
+const root = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: {
+      val: 3,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let result = [];
+  const inorder = (root) => {
+    if (root) {
+      root.left && inorder(root.left);
+      result.push(root.val);
+      root.right && inorder(root.right);
+    }
+  };
+  inorder(root);
+  return result;
+};
+
+inorderTraversal(root);
