72d90231bad50ce3263c725dc5d2ebe7e8c28693
huanxin <heyuycl001@163.com>
2021-04-30 14:15:41 +0800

    198. 打家劫舍

--- leetcode/198. 打家劫舍.js
+++ leetcode/198. 打家劫舍.js
@@ -11,10 +11,10 @@ var rob = function (nums) {
   // 四个房子选最后一个，只能选第一个或者第二个最大的;不选最后一个只能选第1个跟第三个
   // 得出公示
   // fn = Math.max(f(n-2)中最大的 + 最后一个房子, f(n-1)房子中最大的
-  if (nums.length === 0) nums[0];
+  if (nums.length === 0) return 0;
   const dp = new Array(nums.length + 1);
   dp[0] = 0;
-  dp[1] = nums[1];
+  dp[1] = nums[0];
   for (let i = 2; i <= nums.length; i++) {
     dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1]);
   }
